faa4cdc886faaf9c1fab732020eab043219492a3
Whoiamx <gaastontimchuk@gmail.com>
2025-04-13 23:17:56 -0300

    fixes varios

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Header from "./components/Header";
-import { Modal } from "./components/Modal";
 import { ReminderCategoryCards } from "./reminders/components/ReminderCategoryCards";
 
 export default function Home() {
   return (
     <>
       <Header />
+
       <ReminderCategoryCards />
     </>
   );
