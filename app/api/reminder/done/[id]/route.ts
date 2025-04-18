e731cd51beed26c7aeb9f97e45cd425f350463c8
Whoiamx <gaastontimchuk@gmail.com>
2025-04-17 22:53:25 -0300

    solve that

--- app/api/reminder/done/[id]/route.ts
+++ app/api/reminder/done/[id]/route.ts
@@ -1,25 +1,24 @@
 import { prisma } from "@/app/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
-interface Params {
-  params: { id: string };
-}
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
 
-export async function PUT(request: NextRequest, { params }: Params) {
   try {
     const updateReminderToDone = await prisma.reminder.update({
       where: {
-        id: Number(params.id),
+        id: Number(id),
       },
       data: {
         done: true,
       },
     });
 
-    // Return the JSON response
     return NextResponse.json(updateReminderToDone);
   } catch (error) {
-    // Return error message with status code 500
     return NextResponse.json(
       { message: (error as Error).message },
       { status: 500 }
