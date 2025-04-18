6ccc420b5d07d19cc6640b31335c134cb7573887
Whoiamx <gaastontimchuk@gmail.com>
2025-04-17 21:47:42 -0300

    second fix to build

--- app/api/reminder/[id]/route.tsx
+++ app/api/reminder/[id]/route.tsx
@@ -1,11 +1,10 @@
 import { prisma } from "@/app/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
-interface Params {
-  params: { id: string };
-}
-
-export async function GET(request: Request, { params }: Params) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const infoReminder = await prisma.reminder.findMany({
     where: {
       id: Number(params.id),
@@ -15,7 +14,10 @@ export async function GET(request: Request, { params }: Params) {
   return NextResponse.json(infoReminder);
 }
 
-export async function PUT(request: Request, { params }: Params) {
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const { title, description, important, done } = await request.json();
 
@@ -31,12 +33,16 @@ export async function PUT(request: Request, { params }: Params) {
       },
     });
 
-    return NextResponse.json("Se genero correctamente el recordatorio");
+    return NextResponse.json("Se generó correctamente el recordatorio");
   } catch (error) {
-    return NextResponse.json("Surgio un error");
+    return NextResponse.json("Surgió un error");
   }
 }
-export async function DELETE(request: Request, { params }: Params) {
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const deleteReminder = await prisma.reminder.deleteMany({
       where: {
@@ -46,13 +52,13 @@ export async function DELETE(request: Request, { params }: Params) {
 
     return NextResponse.json(
       {
-        message: `Se elimino el registro correctamente`,
+        message: `Se eliminó el registro correctamente`,
       },
       { status: 200 }
     );
   } catch (error) {
     return NextResponse.json(
-      { message: "Surgio un error terrible" },
+      { message: "Surgió un error terrible" },
       { status: 500 }
     );
   }
