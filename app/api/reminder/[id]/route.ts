334ea46b62c1a1079f1cf428d82a11d93a7c0aec
Whoiamx <gaastontimchuk@gmail.com>
2025-04-17 22:28:41 -0300

    22

--- app/api/reminder/[id]/route.ts
+++ app/api/reminder/[id]/route.ts
@@ -1,68 +1,62 @@
 import { prisma } from "@/app/lib/prisma";
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 // ✅ GET: Obtener un recordatorio por ID
 export async function GET(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { id: string } }
 ) {
   const { id } = params;
 
-  const infoReminder = await prisma.reminder.findUnique({
-    where: {
-      id: Number(id), // Convertimos el id a número
-    },
+  // Convertimos el ID a número ya que la base de datos lo requiere como tal
+  const reminder = await prisma.reminder.findUnique({
+    where: { id: Number(id) },
   });
 
-  return NextResponse.json(infoReminder);
+  if (!reminder) {
+    return NextResponse.json(
+      { error: "Recordatorio no encontrado" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(reminder);
 }
 
 // ✅ PUT: Actualizar un recordatorio por ID
 export async function PUT(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { id: string } }
 ) {
-  try {
-    const { id } = params;
-    const { title, description, important, done } = await request.json();
+  const { id } = params;
+  const { title, description, important, done } = await request.json();
 
-    const updated = await prisma.reminder.update({
-      where: {
-        id: Number(id), // Convertimos el id a número
-      },
-      data: {
-        title,
-        description,
-        important,
-        done,
-      },
+  try {
+    const updatedReminder = await prisma.reminder.update({
+      where: { id: Number(id) },
+      data: { title, description, important, done },
     });
 
-    return NextResponse.json(updated);
+    return NextResponse.json(updatedReminder);
   } catch (error) {
-    return NextResponse.json(
-      { message: "Error al actualizar" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: "Error al actualizar" }, { status: 500 });
   }
 }
 
 // ✅ DELETE: Eliminar un recordatorio por ID
 export async function DELETE(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { id: string } }
 ) {
-  try {
-    const { id } = params;
+  const { id } = params;
 
+  try {
     await prisma.reminder.delete({
-      where: {
-        id: Number(id), // Convertimos el id a número
-      },
+      where: { id: Number(id) },
     });
 
     return NextResponse.json({ message: "Eliminado correctamente" });
   } catch (error) {
-    return NextResponse.json({ message: "Error al eliminar" }, { status: 500 });
+    return NextResponse.json({ error: "Error al eliminar" }, { status: 500 });
   }
 }
